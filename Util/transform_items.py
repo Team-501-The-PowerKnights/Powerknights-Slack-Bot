16cb255f1031930e4f3256da04bf226f8b2b365e
Matt-Gleich <matthewgleich@gmail.com>
2019-03-22 22:07:04 -0400

    Update transform_items.py

--- Util/transform_items.py
+++ Util/transform_items.py
@@ -1,12 +1,18 @@
-def lst_str_int(string, seperator):
+import datetime
+
+def week_range():
     """
-    Initialize network tables
-    :parameter string and seperator
-    :return list of ints
+    Get the RFC 3339 timestamps for the start and end of the week
+    :return: lst [start, end] (both ints)
     """
-    lst_strs = string.split("-")
-    lst_ints = []
-    for num in lst_strs:
-        integer_form = int(num)
-        lst_ints.append(integer_form)
-    return lst_ints
+    d = datetime.datetime.today()
+    current_weekday = d.weekday()
+    start_and_end = []
+    days_till_end = 6 - current_weekday
+    now = datetime.datetime(d.year(), d.month(), d.day())
+    start_time = now.isoformat()
+    now_formatted = datetime.datetime.date(d.year(), d.month(), d.day())
+    end_time = now_formatted + datetime.datetime.timedelta(days=days_till_end)
+    start_and_end.append(start_time)
+    start_and_end.append(end_time)
+    return start_and_end
