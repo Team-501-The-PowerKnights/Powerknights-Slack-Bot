a68551a0e3a56555a63dc2565380b0893cc516a1
Matt-Gleich <matthewgleich@gmail.com>
2019-03-22 22:44:18 -0400

    Update date_range.py

--- Util/date_range.py
+++ Util/date_range.py
@@ -6,13 +6,16 @@ def week_range():
     :return: lst [start, end] (both ints)
     """
     d = datetime.datetime.today()
+    print("d = " + str(d))
     current_weekday = d.weekday()
     start_and_end = []
     days_till_end = 6 - current_weekday
-    now = datetime.datetime(d.year(), d.month(), d.day())
+    now = datetime.datetime.date(int(d.year), int(d.month), int(d.day))
     start_time = now.isoformat()
-    now_formatted = datetime.datetime.date(d.year(), d.month(), d.day())
-    end_time = now_formatted + datetime.datetime.timedelta(days=days_till_end)
-    start_and_end.append(start_time)
-    start_and_end.append(end_time)
+    now_formatted = datetime.datetime.date(int(d.year), int(d.month), int(d.day))
+    end_time = now_formatted + datetime.timedelta(days=days_till_end)
+    start_and_end.append(start_time + "Z")
+    start_and_end.append(end_time + "Z")
     return start_and_end
+
+week_range()
