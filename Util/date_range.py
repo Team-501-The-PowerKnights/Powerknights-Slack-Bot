8903c6dbb7c454e5a22cd9549f83384f97da1c5b
Matt-Gleich <matthewgleich@gmail.com>
2019-03-23 21:34:28 -0400

    Fixed append errors

--- Util/date_range.py
+++ Util/date_range.py
@@ -3,19 +3,17 @@ import datetime
 def week_range():
     """
     Get the RFC 3339 timestamps for the start and end of the week
-    :return: lst [start, end] (both ints)
+    :return: lst [start, end] (both strings)
     """
     d = datetime.datetime.today()
-    print("d = " + str(d))
     current_weekday = d.weekday()
-    start_and_end = []
+    start_and_end_dates = []
     days_till_end = 6 - current_weekday
-    now = datetime.datetime.date(int(d.year), int(d.month), int(d.day))
-    start_time = now.isoformat()
-    now_formatted = datetime.datetime.date(int(d.year), int(d.month), int(d.day))
+    now = datetime.date(d.year, d.month, d.day).isoformat()
+    now_formatted = datetime.date(d.year, d.month, d.day)
     end_time = now_formatted + datetime.timedelta(days=days_till_end)
-    start_and_end.append(start_time + "Z")
-    start_and_end.append(end_time + "Z")
-    return start_and_end
+    start_and_end_dates.append(str(now) + "Z")
+    start_and_end_dates.append(str(end_time) + "Z")
+    return start_and_end_dates
 
-week_range()
+print(week_range())
