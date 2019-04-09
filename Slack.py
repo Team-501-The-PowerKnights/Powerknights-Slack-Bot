2be745f755d2a3084c863c4480558726e581154d
Matt-Gleich <matthewgleich@gmail.com>
2019-04-08 20:53:40 -0400

    Updated Slack.py

--- Slack.py
+++ Slack.py
@@ -1,36 +1,11 @@
+import os
 from slackclient import SlackClient
-with open(message.txt)
 
+slack_token = os.environ["xoxb-184987142247-559044913892-m4yXOmQYgtSC4Li0nGqjuwGA"]
+sc = SlackClient(slack_token)
 
-class NoConnection(Exception):
-    pass
-
-class Slacker(object):
-    __token = "<token from account>"  # You need to add the API token from your personal account
-    __connection = None
-    __channel = ["channelone", "channeltwo","general"]
-
-    def __init__(self):
-        try:
-            self.__connection = SlackClient(self.__token)
-        except:
-            raise NoConnection("Could not authenticate with this token")
-
-    def sendMessage(self, channel = "general", message = None):
-        if channel is None:
-            raise NoConnection("You need to specify a valid channel : {}".format(",".join(self.__channel)))
-
-        if message is None:
-            raise NoConnection("You need to specify a message!")
-
-        try:
-            Response = self.__connection.api_call("channel.postMessage", channel=channel, message=message)
-        except:
-            raise NoConnection("Could not send the message to Slack!")
-
-        return Response
-
-if __name__ == "__message__":
-    SCLK = Slacker()
-    SCLK.sendMessage(channel = "Channel to send", message = "Message to send!")
-    SCLK.sendMessage(message = "Message to send!")
+sc.api_call(
+  "chat.postMessage",
+  channel="bot-dev",
+  text="Testing Testing"
+)
