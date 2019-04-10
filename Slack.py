1dfa8d243e05244bcfa0fbe6ec045acf48fcdded
Matt-Gleich <matthewgleich@gmail.com>
2019-04-10 14:35:39 -0400

    Working Version

--- Slack.py
+++ Slack.py
@@ -1,11 +1,24 @@
+try:
+    with open("slack_token.txt") as slack_token_file:
+        slacktoken_invalid = str(slack_token_file.read())
+        slacktoken = slacktoken_invalid.strip("\n")
+
+except FileNotFoundError:
+    print("Please make the slack_token.txt file and put your API key in it")
+
+channels = {
+           "polls":"CE36EEKA9",
+           }
+
+
 import os
 from slackclient import SlackClient
 
-slack_token = os.environ["xoxb-184987142247-559044913892-m4yXOmQYgtSC4Li0nGqjuwGA"]
-sc = SlackClient(slack_token)
+sc = SlackClient(slacktoken)
 
 sc.api_call(
   "chat.postMessage",
-  channel="bot-dev",
-  text="Testing Testing"
+  channel="CE36EEKA9",
+  text="Testing Testing",
+  as_user="false"
 )
