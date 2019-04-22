3410e13da411f4997c3792f228fb81e40155b7d1
Matt-Gleich <matthewgleich@gmail.com>
2019-04-21 21:19:51 -0400

    Zulu Time Appended

--- date_range.py
+++ date_range.py
@@ -29,6 +29,7 @@ def week_range():  # Currently working version (Missing start)
     Get the RFC 3339 timestamps for the start and end of the week
     :return: lst
     """
+    zulu_time = "Z"
     dates = []
     current_day = datetime.date.today()
     current_weekday = current_day.weekday()
@@ -37,14 +38,16 @@ def week_range():  # Currently working version (Missing start)
     start_date_str = str(current_day - start_delta)
     start_date_lst = str_lst_num(start_date_str, "-", True)
     start_date = datetime.datetime(start_date_lst[0], start_date_lst[1], start_date_lst[2]).isoformat()
-    dates.append(start_date)
+    start_date_zulu = "".join([start_date, zulu_time])
+    dates.append(start_date_zulu)
     # Finding end of the week:
     days_till_end = 6 - current_weekday
     end_delta = datetime.timedelta(days=days_till_end)
     end_date_str = str(current_day + end_delta)
     end_date_lst = str_lst_num(end_date_str, "-", True)
     end_date = datetime.datetime(end_date_lst[0], end_date_lst[1], end_date_lst[2]).isoformat()
-    dates.append(end_date)
+    end_date_zulu = "".join([end_date, zulu_time])
+    dates.append(end_date_zulu)
     return dates
 
 
