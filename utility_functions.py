eeaa095bc0ce78f93eea251e69e83fd1524e3caf
Matt-Gleich <matthewgleich@gmail.com>
2019-04-28 18:57:15 -0400

    Finish iso_extract_info function

--- utility_functions.py
+++ utility_functions.py
@@ -4,7 +4,9 @@ def iso_extract_info(string):
     """
     Will get all of the info and return it as an array
     :param string: ISO formatted string that will be used for extraction
-    :return: array [year, month, day, military_time_hour, hour, minutes, seconds] (do note that all are ints except for the minutes and seconds.)
+    :return: array [year, month, day, military_time_hour, minutes, hours]
+        :note: every item is an int except for minutes
+        :note: hours only is there is military_time_hour is greater than 12
     """
     elements = []
     characters = list(string)
@@ -14,14 +16,22 @@ def iso_extract_info(string):
     military_time_hours_int = int("".join(characters[11:13]))
     minutes_int = "".join(characters[14:16])
     hours = 0
-    if military_time_hours_int > 12:
-        hours += military_time_hours_int - 12
     elements.append(year_int)
     elements.append(month_int)
     elements.append(day_int)
-    elements.append(h)
+    elements.append(minutes_int)
+    if military_time_hours_int > 12:
+        hours += military_time_hours_int - 12
+        elements.append(hours)
+    return elements
 
 
+# Testing:
+print("[year, month, day, military_time_hour, minutes, hours]")
+print(iso_extract_info('2019-04-27T16:00:00-04:00'))
+
+
+# Doesn't use the "iso_extract_info" function
 def iso_format_to_regular(string):
     """
     Will take a string that is an iso formatted string and make it look readable
@@ -47,6 +57,7 @@ def iso_format_to_regular(string):
 # print(iso_format_to_regular('2019-04-27T16:00:00-04:00'))
 
 
+# Doesn't use the "iso_extract_info" function
 def multiday_checker_ISO(start_date, end_date):
     """
     Will check if an event is more than one day long
@@ -66,6 +77,7 @@ def multiday_checker_ISO(start_date, end_date):
         return True
 
 
+# Doesn't use the "iso_extract_info" function
 def fix_time(strange_date):
     """
     Will rearrange the strange date that Google gives and repalce it with the normal string.
@@ -80,6 +92,7 @@ def fix_time(strange_date):
     return new_str
 
 
+# Doesn't use the "iso_extract_info" function
 def multiday_checker_STRANGE(start_date, end_date):
     """
     Will check if an event is more than day long
@@ -108,6 +121,7 @@ def multiday_checker_STRANGE(start_date, end_date):
 # print(multiday_checker_STRANGE('2019-04-21', '2019-04-22'))
 
 
+# Doesn't use the "iso_extract_info" function
 def STRANGE_string_weekday(string):
     """
     Will take a string that is a date formatted in the Google format and find what day of the week it is
@@ -142,6 +156,7 @@ def STRANGE_string_weekday(string):
 # print(STRANGE_string_weekday("2019-04-27"))
 
 
+# Doesn't use the "iso_extract_info" function
 def ISO_string_weekday(string):
     """
     Will take a string that is a date formatted in the ISO format and find what day of the week it is
