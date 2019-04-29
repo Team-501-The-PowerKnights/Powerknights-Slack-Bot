6377ab93b0c0657a1aa9cb226262aa71d080fae0
Matt-Gleich <matthewgleich@gmail.com>
2019-04-29 00:47:28 -0400

    Update Message_Creator.py

--- Message_Creator.py
+++ Message_Creator.py
@@ -1,9 +1,5 @@
 import utility_functions as UF
 
-"""
-Week day key
-"""
-
 def create_message(events, get_info):
     """
     Creates the message that will be posted to slack based of the events from the google calendar
@@ -15,85 +11,104 @@ def create_message(events, get_info):
         :type: string
         :type: array (index 0=messsage and index 1=used_emojis) this is only used if get_info is True
     """
-    starter_string = \
-    """
-    @everyone
-    *What days wil you be coming to this week? Please react with the corresponding emoji:*
-    Link to calendar = PUT LINK HERE
-    """
+    seperator = "---------------------------------------------"
+    starter_string_lst = [
+    seperator,
+    "@channel",
+    "*What days wil you be coming to this week? Please react with the corresponding emoji:*",
+    "Link to calendar: "
+    ]
+    starter_string = "\n".join(starter_string_lst)
     emojis = [
-    ":one:",":two:",":three:",
-    ":four:",":five:",":six:",
-    ":seven:",":eight:",":nine:",
-    ":keycap_ten:",":100:",":capital_abcd:",
-    ":abcd:",":1234:",":symbols:",
-    ':a:', ':ab:', ':b:',
-    ':cl:', ':cool:', ':free:',
-    ':information_source:', ':id:', ':m:',
-    ':new:', ':ng:', ':o2:',
-    ':ok:', ':parking:', ':sos:',
-    ':up:', ':vs:', ':koko:', ':sa:',
-    ':u6708:',
+    "one","two","three",
+    "four","five","six",
+    "seven","eight","nine",
+    "keycap_ten","100","capital_abcd",
+    "abcd","1234","symbols",
+    'a', 'ab', 'b',
+    'cl', 'cool', 'free',
+    'information_source', 'id', 'm',
+    'new', 'ng', 'o2',
+    'ok', 'parking', 'sos',
+    'up', 'vs', 'koko', 'sa',
+    'u6708',
     ]
     used_emojis = []
-    if events < 35:
-        seperator = "---------------------------------------------"
+    if len(events) < 35:
+        with open("message.txt", "w") as message_file:
+            message_file.write(starter_string)
+            message_file.write(seperator + "\n")
         events_str = [starter_string]
-        number_of_events = 0
         for event in events:
-            number_of_events += 1
             event_name = event['summary']
             description = event['description']
             location = event['location']
-            start_time_raw = event['start']['datetime']
-            end_time_raw = event['end']['datetime']
-            if len(start_time_raw) < 25:
+            try:
+                start_time_raw = event['start']['dateTime']
+            except KeyError:
+                start_time_raw = event['start']['date']
+            try:
+                end_time_raw = event['end']['dateTime']
+            except KeyError:
+                end_time_raw = event['end']['date']
+            if len(start_time_raw) < 25:  # Checking if its an all day event
                 all_day_event = True
-                start_time = UF.fix_time(len(start_time_raw))
-                end_time = UF.fix_time(len(end_time_raw))
+                start_time = UF.fix_time(start_time_raw)
+                end_time = UF.fix_time(end_time_raw)
                 multiday = UF.multiday_checker_STRANGE(start_time_raw, end_time_raw)
                 if multiday:
                     emoji = emojis.pop(0)
                     used_emojis.append(emoji)
                     strings = [
-                    seperator,
                     "_*{name}*_ = {sign}".format(name=event_name,sign=emoji),
                     description,
-                    "{start} - {end}".format(start=start_time,end=end_time)
+                    "{start} - {end}".format(start=start_time,end=end_time),
                     seperator
                     ]
-                    with open("message.txt") as message_file:
+                    with open("message.txt", "a") as message_file:
                         for line in strings:
-                            message_file.write(line,"\n")
+                            message_file.write(line + "\n")
                 else:
-                    day_of_week =
+                    day_of_week = UF.STRANGE_string_weekday(start_time)
                     emoji = emojis.pop(0)
                     used_emojis.append(emoji)
                     strings = [
-                    seperator,
-                    "_*{name}*_ = {sign}".format(name=event_name,sign=emoji),
-                    description,
-                    "{start} - {end}".format(start=start_time,end=end_time)
+                    "_*{weekday}*_ = {sign}".format(weekday=day_of_week,sign=emoji),
+                    event_name,
+                    start_time,
                     seperator
                     ]
-                    with open("message.txt") as message_file:
+                    with open("message.txt", "a") as message_file:
                         for line in strings:
-                            message_file.write(line,"\n")
+                            message_file.write(line + "\n")
             else:
+                day_of_week = UF.ISO_string_weekday(start_time_raw)
+                emoji = emojis.pop(0)
+                used_emojis.append(emoji)
                 start_time = UF.iso_format_to_regular(start_time_raw)
                 end_time = UF.iso_format_to_regular(end_time_raw)
-                multiday = UF.multiday_checker_ISO(start_time_raw, end_time_raw)
                 strings = [
-                seperator,
-
+                "_*{weekday}*_ = {sign}".format(weekday=day_of_week,sign=emoji),
+                event_name,
+                "{start} - {end}".format(start=start_time,end=end_time),
+                seperator
                 ]
-        if get_info:
-            return_information = []
-            return_information.append(final_message)
-            return_information.append(used_emojis)
-            return return_information
-        else:
-            return final_message
+                with open("message.txt", "a") as message_file:
+                    for line in strings:
+                        message_file.write(line + "\n")
     else:
         print("Error:")
-        print("This bot currently doesn't support more than 35 events. Please put an issue on github that you would like this to be increased.")
+        print("This bot currently doesn't support more than 35 events in one week. Please put an issue on github that you would like this to be increased.")
+    with open("message.txt") as message_file:
+        final_message = message_file.read()
+    if get_info:
+        return_information = []
+        return_information.append(final_message)
+        return_information.append(used_emojis)
+        return return_information
+    else:
+        return final_message
+
+
+# Testing:
+# print(create_message(""))
