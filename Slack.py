de104032ab5c5922f7511005c1fe0fcfe2b8d621
Matt-Gleich <matthewgleich@gmail.com>
2019-04-29 00:47:25 -0400

    Update Slack.py

--- Slack.py
+++ Slack.py
@@ -1,23 +1,8 @@
 import os
 from slackclient import SlackClient
 
-try:
-    with open("slack_token.txt") as slack_token_file:
-        slacktoken_invalid = str(slack_token_file.read())
-        slacktoken = slacktoken_invalid.strip("\n")
-except FileNotFoundError:
-    print("Please make the slack_token.txt file and put your Slack API key in it")
 
-channels = {
-           "polls":"CE36EEKA9",
-           "general":"C5DEPCK24",
-           "bot-dev":"CGE8GGJ0G",
-           }
-
-sc = SlackClient(slacktoken)
-
-
-def send_message(channel_loc,message,get_info):
+def send_message(key, channel_loc,message,get_info):
     """
     Bot will send a message to Slack
     :param channel where the message wants to be send.
@@ -29,6 +14,7 @@ def send_message(channel_loc,message,get_info):
     :return will return the action's data only if get_info is True
         :type dictonary
     """
+    sc = key
     action = sc.api_call(
     "chat.postMessage",
     channel=channel_loc,
@@ -43,7 +29,7 @@ def send_message(channel_loc,message,get_info):
         return action
 
 
-def emoji_react(emoji,message,get_info):
+def emoji_react(key, emoji,message,get_info):
     """
     Bot will react with an emoji to a message in Slack
     :param emoji that will be reacted
@@ -55,6 +41,7 @@ def emoji_react(emoji,message,get_info):
     :return will return the action's data only if get_info_is True
         :type
     """
+    sc = key
     action = sc.api_call(
     "reactions.add",
     channel=message["channel"],
@@ -64,7 +51,21 @@ def emoji_react(emoji,message,get_info):
     if get_info:
         return action
 
-message_from_file = "Testing Testing"
 
-response = send_message(channels["bot-dev"],message_from_file,True)
-emoji_reaction = emoji_react("thumbsup",response,False)
+# Testing:
+# message_from_file = "Testing Testing"
+# try:
+#     with open("slack_token.txt") as slack_token_file:
+#         slacktoken_invalid = str(slack_token_file.read())
+#         slacktoken = slacktoken_invalid.strip("\n")
+# except FileNotFoundError:
+#     print("Please make the slack_token.txt file and put your Slack API key in it")
+# sc = SlackClient(slacktoken)
+# channels = {
+#            "polls":"CE36EEKA9",
+#            "general":"C5DEPCK24",
+#            "bot-dev":"CGE8GGJ0G",
+#            }
+#
+# response = send_message(sc, channels["bot-dev"],message_from_file,True)
+# emoji_reaction = emoji_react(sc, "thumbsup",response,False)
