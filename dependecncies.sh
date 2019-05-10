7f6b74318d0cd5ab36ce7898adc8c790da6f9778
Matthew Gleich <43759105+Matt-Gleich@users.noreply.github.com>
2019-05-09 22:25:09 -0400

    Update dependecncies.sh

--- dependecncies.sh
+++ dependecncies.sh
@@ -1,5 +1,3 @@
 pip install --upgrade
 
-pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib
-
-pip install slackclient
+pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib slackclient
