12d5095175ba924ea11bb1648c350dc2a629925a
Matthew Gleich <matthewgleich@gmail.com>
2019-07-19 23:13:06 -0400

    Add message confirmation

--- main.py
+++ main.py
@@ -33,11 +33,22 @@ def main():
     emojis = message_info[1]
 
     # Post message to Slack
-    posted_message = SL.send_message(sc,channels["polls"],message_to_post,True)
+    posted_message = SL.send_message(sc,channels["bot-dev"],message_to_post,True)
 
     # React the emojis to Slack
     for emoji in emojis:
         SL.emoji_react(sc,emoji,posted_message,False)
+
+    correct_message = input("Did the message look ok in the #polls channel? If you answer with 'y' it will be posted in the polls channel. If you answer 'gen' it will be posted in general. If you answer with 'n' or anything other than the commands shown before, then it will be stopped and not posted any where else.\n")
+
+    if "gen" in correct_message.lower():
+        posted_message = SL.send_message(sc,channels["general"],message_to_post, True)
+        for emoji in emojis:
+            SL.emoji_react(sc,emoji,posted_message,False)
+    elif "y" in correct_message.lower():
+        posted_message = SL.send_message(sc,channels["polls"],message_to_post, True)
+        for emoji in emojis:
+            SL.emoji_react(sc,emoji,posted_message,False)
 
 
 main()
